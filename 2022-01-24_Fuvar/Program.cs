89b624a0164280705807cb89779c0a551d86565b
csirkep <csirkep.12d@acsjszki.hu>
2022-01-31 10:36:23 +0100

    5. feladat nem befejezve

--- 2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/Program.cs
@@ -17,7 +17,7 @@ namespace _2022_01_24_Fuvar
                 fuvarok.Add(new Fuvar(sor));
             }
             Console.WriteLine($"3. feladat : {fuvarok.Count} fuvar");
-
+            //4.
             double Bevétel = 0;
             int db = 0;
             foreach (var f in fuvarok)
@@ -28,6 +28,35 @@ namespace _2022_01_24_Fuvar
                     db++;
                 }
                 Console.WriteLine($"4.feladat: {db} fuvar alatt: {Bevétel}");
+                
+                //5.
+                int bankkártyás = 0;
+                int készpénz = 0;
+                foreach (var h in fuvarok)
+                {
+                    if (h.FizetésMód == "bankkártyás")
+                    {
+                        bankkártyás++;
+                    }
+                    if (h.FizetésMód == "készpénz")
+                    {
+                        készpénz++;
+                    }
+                }
+                
+                //5.b
+                Dictionary<string, int> stat = new Dictionary<string, int>();
+                foreach (var d in fuvarok)
+                {
+                    if (stat.ContainsKey(d.FizetésMód))
+                    {
+                        stat[d.FizetésMód]++;
+                    }
+                    else
+                    {
+                        stat.Add(d.FizetésMód, 1);
+                    }
+                }
 
 
                 Console.ReadKey();
