b0ec450c48a711fabb7369075f158f5acbfe4591
csirkep <csirkep.12d@acsjszki.hu>
2022-01-31 10:11:13 +0100

    3.

--- 2022-01-24_Fuvar/Program.cs
+++ 2022-01-24_Fuvar/Program.cs
@@ -11,6 +11,12 @@ namespace _2022_01_24_Fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            foreach (var sor in File.ReadAllLines("fuvar.csv").Skip(1))
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+            Console.WriteLine($"3. feladat : {fuvarok.Count} fuvar");
         }
     }
 }
